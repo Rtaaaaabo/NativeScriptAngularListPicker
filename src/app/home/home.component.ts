9f6df4f8d6e71b1c3ec70e438dd028a4b3b9f19e
Taku Nakagawa <taku_nakagawa@change-jp.com>
2019-02-03 00:46:16 +0900

    Controller側の実装

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { ListPicker } from "tns-core-modules/ui/list-picker";
 
-let divisions = ["北海道", "秋田", "青森", "岩手", "宮城", "山形", "福島", "茨木", "栃木", "群馬", "埼玉", "千葉", "東京", "神奈川", "新潟", "富山"];
+let divisionsList = ["北海道", "秋田", "青森", "岩手", "宮城", "山形", "福島", "茨木", "栃木", "群馬", "埼玉", "千葉", "東京", "神奈川", "新潟", "富山"];
 
 @Component({
     selector: "Home",
@@ -10,11 +10,21 @@ let divisions = ["北海道", "秋田", "青森", "岩手", "宮城", "山形", 
 })
 export class HomeComponent implements OnInit {
 
+    divisions : Array<string> = [];
+    picked : string;
+
     constructor() {
-        // Use the component constructor to inject providers.
+        for(let i=0; i<divisionsList.length; i++) {
+            this.divisions.push(divisionsList[i]);
+        }
+    }
+
+    selectedIndexChangeed(args) {
+        let picker = <ListPicker>args.object;
+        this.picked = this.divisions[picker.selectedIndex];
     }
 
     ngOnInit(): void {
-        // Init your component properties here.
+        console.log("都道府県のListPickerです！");
     }
 }
